51f2a7f16a826ba1dc3814c625d7541dfc7a63b3
Anil Keshavamurthy <32092397+anilkeshavamurthy@users.noreply.github.com>
2019-10-22 11:03:21 +0530

    Update terraform_backend.tf
    Spelling mistake

--- modules/initial-setup/terraform_backend.tf
+++ modules/initial-setup/terraform_backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-maf"
-    key    = "moduals/initial/terraform.tfstate"
+    key    = "modules/initial/terraform.tfstate"
     region = "us-east-1"
   }
 }
